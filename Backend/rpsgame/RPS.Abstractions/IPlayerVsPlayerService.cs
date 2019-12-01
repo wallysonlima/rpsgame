4836a4a1e90561995d942ffac8e266b391e89a99
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-12-01 14:14:47 -0300

    Solved import dependency problems, with errors

--- Backend/rpsgame/RPS.Abstractions/IPlayerVsPlayerService.cs
+++ Backend/rpsgame/RPS.Abstractions/IPlayerVsPlayerService.cs
@@ -1,5 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace RPS.Abstractions
 {
+    using System;
+    using System.IO;
+    using Microsoft.AspNetCore.Http;
+    using RPS.Domain.DTO;
     public interface IPlayerVsPlayerService
     {
         Task<IEnumerable<PlayerDTO>> rps_game_winner(PlayerVsPlayerRequest req);
